23841f3da992de98f4e150f8c0e08a3d302d656d
Riri0000 <ririko010102@gmail.com>
2020-12-04 13:29:51 +0900

    Add: ログイン機能の追加

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,22 @@
+class UserSessionsController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
+  def new; end
+
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      # 仮実装でroot_pathへ遷移。最終形はポスト一覧画面。
+      redirect_back_or_to root_path, success: 'ログインしました'
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to login_path, notice: 'ログアウトしました'
+  end
+end
