acb458f25c6fccdaaff1e1c14fa75477d1127715
Riri0000 <ririko010102@gmail.com>
2020-12-07 18:38:39 +0900

    Add: UserProfile create部分追加

---
+++ app/controllers/user_profiles_controller.rb
@@ -0,0 +1,25 @@
+class UserProfilesController < ApplicationController
+  def new
+    @user_profile = UserProfile.new
+  end
+
+  def create
+    @user_profile = current_user.build_user_profile(user_profile_params)
+    if @user_profile.save
+      redirect_to user_profile_path(@user_profile), success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :new
+    end
+  end
+
+  def show
+    @user_profile = UserProfile.find(params[:id])
+  end
+
+  private
+
+  def user_profile_params
+    params.require(:user_profile).permit(:age, :job, :total_assets, :annual_income, :monthly_income, :household_member, :message)
+  end
+end
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,3 +1,6 @@
 class UserProfile < ApplicationRecord
   belongs_to :user
+
+  validates :job, length: { maximum: 20 }
+  validates :message, length: { maximum: 144 }
 end
