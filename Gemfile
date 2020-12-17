520f337f387261f08fbcad43a9dd6df2beafbf35
Riri0000 <ririko010102@gmail.com>
2020-12-17 09:35:47 +0900

    Add: chartkickのインストール

--- Gemfile
+++ Gemfile
@@ -75,6 +75,9 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
+# グラフ生成用
+gem "chartkick"
+
 # slim
 gem 'slim-rails'
 gem 'html2slim'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("jquery")
 require("@nathanvda/cocoon")
 require("bootstrap");
 require("@fortawesome/fontawesome-free");
+require("chartkick")
+require("chart.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
