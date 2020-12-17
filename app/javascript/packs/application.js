533dca4e96c2dc0a7cc82b0e77930b8ea9674db0
Riri0000 <ririko010102@gmail.com>
2020-12-17 09:05:35 +0900

    Fix: Bootstrapの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require("@nathanvda/cocoon")
+require("bootstrap");
+require("@fortawesome/fontawesome-free");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
