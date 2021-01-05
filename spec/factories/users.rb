518ed025a6563852607716f44b56cbe52eb98ae2
Riri0000 <ririko010102@gmail.com>
2021-01-05 16:12:41 +0900

    Add: rails_adminシステムスペック

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,14 @@
 FactoryBot.define do
-  factory :user, aliases: [:owner] do
-    nickname { 'SampleUser' }
+  factory :user do
+    nickname { Faker::Name.name }
     sequence(:email) { |n| "sample#{n}@example.com" }
+    role { :general }
     password { 'password' }
     password_confirmation { 'password' }
+  end
+
+  trait :admin do
+    sequence(:nickname) { |n| "admin-#{n}" }
+    role { :admin }
   end
 end
