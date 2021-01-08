f5f802cd326f8a897e014e699944889cecc9fc09
Riri0000 <ririko010102@gmail.com>
2021-01-08 21:03:06 +0900

    Update: いいね機能のN + 1問題解消

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
   belongs_to :user
-  belongs_to :account_book
+  belongs_to :account_book, counter_cache: :likes_count
   validates :user_id, uniqueness: { scope: :account_book_id }
 end
