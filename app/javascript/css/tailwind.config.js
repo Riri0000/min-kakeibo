43715a11ad36d32d3d882e1e45b2a2433e41ec43
Riri0000 <ririko010102@gmail.com>
2021-01-15 22:11:19 +0900

    Add: Tailwindのpurge設定を追加

--- app/javascript/css/tailwind.config.js
+++ app/javascript/css/tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './app/**/*.html.erb',
+    './app/**/*.html.slim',
+    './app/**/*.js.erb',
+    './app/helpers/**/*.rb',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
