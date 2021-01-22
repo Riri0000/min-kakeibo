a9cb4595e5cc46473a608a71b01522b4529a8def
Riri0000 <ririko010102@gmail.com>
2021-01-22 23:42:31 +0900

    Fix:CSS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,8 @@ const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
 
 // CSS----------------------
-import '../stylesheets/application.scss';
 import '../css/tailwind.scss';
+import '../css/toastr.scss';
 
 window.toastr = require('toastr')
 window.jQuery = window.$ = require('jquery')
